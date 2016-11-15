41c8aa674cf59f5380222907719df7a35f01dbc7
Dmitry Matlah <mclaod@gmail.com>
2016-11-15 23:03:54 +0300

    Initial

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: THUNDERBOLT
+ * Date: 15.11.2016
+ * Time: 22:41
+ */
+global $config;
+$config = [
+  'domain' => $_SERVER['SERVER_NAME']
+];
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: THUNDERBOLT
+ * Date: 01.11.2016
+ * Time: 22:52
+ */
+require 'vendor/autoload.php';
+global $config;
+var_dump($config);
+echo 'Добро пожаловать на '.$config['domain'];
